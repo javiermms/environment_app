65ec9db1b46f23f6f92d99ce20b6f289c1072977
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-12-05 13:19:46 -0800

    👷🏾‍♀️ removed old profile query routes to make way for new authenicated routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,22 +41,6 @@ module.exports = function (app) {
         res.render('login')
     })
 
-    // LOGIN POST
-    app.post('/login', (req, res) => {
-        const query = { username: req.body.username }
-        Profile.findOne(query).then((profile) => {
-            console.log("profile:", profile)
-            if (profile != null | profile != undefined) {
-                res.redirect(`/profiles/${profile._id}`)
-            } else {
-                let alreadyUser = false;
-                console.log("no user")
-                res.redirect(`/login`)
-            }
-        }).catch((err) => {
-            console.log(err.message);
-        });
-    });
 
     // LOGOUT
     app.get('/logout', (req, res) => {
@@ -64,3 +48,51 @@ module.exports = function (app) {
         res.redirect('/');
     });
 }
+
+
+/*
+*
+// OLD LOGIN POST
+app.post('/login', (req, res) => {
+    const query = { username: req.body.username }
+    Profile.findOne(query).then((profile) => {
+        console.log("profile:", profile)
+        if (profile != null | profile != undefined) {
+            res.redirect(`/profiles/${profile._id}`)
+        } else {
+            let alreadyUser = false;
+            console.log("no user")
+            res.redirect(`/login`)
+        }
+    }).catch((err) => {
+        console.log(err.message);
+    });
+});
+
+// CREATE (TODO: Change to /SIGN-UP add real auth and move to AUTH)
+app.post('/profiles', (req, res) => {
+    const query = { username: req.body.username }
+    console.log(query)
+    Profile.findOne(query).then((profile) => {
+        let userExists = req.body._id;
+        console.log(userExists);
+        // check if the username already exists in database
+        if (req.body._id) {
+            Profile.findById(req.params.id, (err, profile) => {
+                console.log(req.params.id)
+                let userExists = true
+                res.redirect(`/form`);
+            }).catch((err) => {
+                console.log(err.message);
+            })
+        // if it doesn't, then create profile and render it
+        } else {
+            Profile.create(req.body).then((profile) => {
+              res.redirect(`/profiles/${profile._id}`);
+            }).catch((err) => {
+              console.log(err.message);
+              })
+          }
+      })
+  })
+*/
--- controllers/profiles.js
+++ controllers/profiles.js
@@ -10,33 +10,6 @@ module.exports = function auth(app) {
     res.render('index');
   });
 
-  // CREATE (TODO: Change to /SIGN-UP add real auth and move to AUTH)
-  app.post('/profiles', (req, res) => {
-      const query = { username: req.body.username }
-      console.log(query)
-      Profile.findOne(query).then((profile) => {
-          let userExists = req.body._id;
-          console.log(userExists);
-          // check if the username already exists in database
-          if (req.body._id) {
-              Profile.findById(req.params.id, (err, profile) => {
-                  console.log(req.params.id)
-                  let userExists = true
-                  res.redirect(`/form`);
-              }).catch((err) => {
-                  console.log(err.message);
-              })
-          // if it doesn't, then create profile and render it
-          } else {
-              Profile.create(req.body).then((profile) => {
-                res.redirect(`/profiles/${profile._id}`);
-              }).catch((err) => {
-                console.log(err.message);
-                })
-            }
-        })
-    })
-
   // SHOW
   app.get('/profiles/:id', (req, res) => {
     Profile.findById(req.params.id, (err, profile) => {
